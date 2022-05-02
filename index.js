5975d1de284a644d78a7f8a876d251f8988eb621
Bubbly <68120990+BubblyDisc@users.noreply.github.com>
2022-05-02 09:22:26 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+app.get('/', async (req, res) => {
+    await delay(1000)
+
     res.redirect('https://bladelist.gg/')
 })
 
-app.get('*', (req, res) => {
+app.get('*', async (req, res) => {
+    await delay(1000)
+
     res.redirect('https://bladelist.gg/')
 })
-
 
 app.listen(process.env.PORT, () => {
     console.log(`Bladelist.gg redirect listening on port ${process.env.PORT}`)
