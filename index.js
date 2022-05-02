a1ac784e649a5274f714d98ee291128895e1563e
Bubbly <68120990+BubblyDisc@users.noreply.github.com>
2022-05-02 08:46:17 +0000

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.get('*', (req, res) => {
     res.redirect('https://bladelist.gg/')
 })
 
+
 app.listen(process.env.PORT, () => {
     console.log(`Bladelist.gg redirect listening on port ${process.env.PORT}`)
 })
